9346239cd724667455af54fbeebb505b4e672e31
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-11 03:40:07 -0700

    HW07_ch10_ex02.py_v01

--- HW07_ch10_ex02.py
+++ HW07_ch10_ex02.py
@@ -5,3 +5,22 @@
 # In your final submission:
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+def capitalize_nested(list_str):
+	count = 0
+	for item in list_str:
+		if isinstance(item, list):
+			list_str[count] = capitalize_nested(item)
+		else:
+			list_str[count] = item.upper()
+		count += 1
+	return list_str
+
+def main():
+	list_str = ['apple', ['bear', 'python'], 'cat']
+	print(capitalize_nested(list_str))
+
+if __name__ == '__main__':
+	main()
+		
+			
