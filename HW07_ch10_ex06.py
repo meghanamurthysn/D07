07e3ee80754e2fafc940e10d62ebf7fa0857e610
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-11 03:41:42 -0700

    HW07_ch10_ex06.py_v01

--- HW07_ch10_ex06.py
+++ HW07_ch10_ex06.py
@@ -3,3 +3,13 @@
 # In your final submission:
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+def is_sorted(lst):
+	return lst == sorted(lst)
+
+def main():
+	print(is_sorted([3, 2, 1]))
+	print(is_sorted([1, 2, 2]))
+
+if __name__ == '__main__'	:
+	main()
